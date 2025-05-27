8cdc368012371392f7857fd29fbb226113913f35
Mateo López Campo <matlopezca@unal.edu.co>
2025-05-27 14:39:57 -0500

    lab 7
    matplot

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt # type: ignore
+
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
@@ -16,3 +20,87 @@ def pregunta_01():
     El gráfico debe salvarse al archivo `files/plots/news.png`.
 
     """
+
+    df = pd.read_csv('files/input/news.csv', encoding='utf-8', index_col=0, sep=',')
+
+    plt.Figure()
+
+    media_colors = {
+        'Television': 'dimgray',
+        'Newspaper': 'grey',
+        'Internet': 'tab:blue',
+        'Radio': 'lightgrey'
+    }
+
+    media_zorder = {
+        'Television': 1,
+        'Newspaper': 1,
+        'Internet': 2,
+        'Radio': 1
+    }
+
+    media_linewidths = {
+        'Television': 2,
+        'Newspaper': 2,
+        'Internet': 3,
+        'Radio': 2
+    }
+
+    for media in df.columns:
+        plt.plot(df[media],
+                color=media_colors[media],
+                label=media,
+                zorder=media_zorder[media],
+                linewidth=media_linewidths[media]
+                )
+
+    plt.title('How people get their news', fontsize=16)
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().axes.get_yaxis().set_visible(False)
+
+    for media in df.columns:
+        first_year = df.index[0]
+        plt.scatter(
+            x=first_year,
+            y=df[media][first_year],
+            color=media_colors[media],
+            zorder=media_zorder[media]
+        )
+
+        plt.text(
+            first_year - 0.2,
+            df[media][first_year],
+            media + " " + str(df[media][first_year]) + "%",
+            ha='right',
+            va='center',
+            color=media_colors[media]
+        )
+
+        last_year = df.index[-1]
+        plt.scatter(
+            x=last_year,
+            y=df[media][last_year],
+            color=media_colors[media],
+            zorder=media_zorder[media]
+        )
+
+        plt.text(
+            last_year + 0.2,
+            df[media][last_year],
+            str(df[media][last_year]) + "%",
+            ha='left',
+            va='center',
+            color=media_colors[media]
+    )
+            
+
+    output_dir = "files/plots"
+    os.makedirs(output_dir, exist_ok=True)
+
+
+    plt.savefig(os.path.join(output_dir, "news.png"))
+    plt.close()
+
+pregunta_01()
